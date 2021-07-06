9e30f53fe93c23ce51a073405666fe11e9cd0ebe
MarcoDeRosso <mcoder@hotmail.it>
2021-07-06 11:11:40 +0200

    v-for stamp

---
+++ js/script.js
@@ -0,0 +1,22 @@
+Vue.config.devtools = true;
+
+new Vue({
+    el: '#app',
+    data: {
+        jobs: [{
+            id: 1,
+            company: 'Perferendis',
+            position: 'Developer',
+            description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+            created_at: '5/22/2021',
+            logo: 'logo.jpg',
+            city: 'Roma',
+            contract: 'Full Time'
+        }],
+        starred: [1, 2, 3],
+        applied: [4, 5]
+    },
+    methods: {
+
+    }
+});
